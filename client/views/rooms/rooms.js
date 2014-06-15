ddd52b8a8bebdae5422962c5c6d635e3a72352d9
Chris Magnacca <chrismagnacca@gmail.com>
2014-06-15 15:20:19 -0400

    added message input into the refactor

--- client/views/rooms/rooms.js
+++ client/views/rooms/rooms.js
@@ -1,2 +1,17 @@
-Template.rooms.events({  
+Template.rooms.events({
+  "submit form#new-message": function(e) {
+    e.preventDefault();
+
+    var username = Meteor.user().username;
+    var currentRoom = Session.get("currentRoom").toString();
+    var inputMessage = $("input#message-to-add").val();
+
+    Messages.insert({ username: username, message: inputMessage, room: currentRoom });
+
+    if(Rooms.find({number: currentRoom}).count() == 0)
+      Rooms.insert({number: currentRoom});
+
+    Meteor.call("ScrollTop");
+    $("input#message-to-add").val("");
+  }
 });
