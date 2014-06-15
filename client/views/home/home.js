0984da1194798d134b1ef2875ffc9ee864d32dbf
Chris Magnacca <chrismagnacca@gmail.com>
2014-06-15 16:02:34 -0400

    added logic around room creation

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,2 +1,13 @@
 Template.home.events({
+  "click #room-to-add": function(e) {
+    e.preventDefault();
+    //TODO: refactor modals for login/register into single modal
+    //      and include create room.
+  }
+});
+
+Template.home.helpers({
+  noRoomsCreated: function() {
+    return (Rooms.find({}).count() == 0);
+  }
 });
