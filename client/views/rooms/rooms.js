616f5e72fcc1c080c0cabbfd82446fd045b16c11
Chris Magnacca <chrismagnacca@gmail.com>
2014-06-15 19:08:21 -0400

    refactoring for the roomCreate methods

--- client/views/rooms/rooms.js
+++ client/views/rooms/rooms.js
@@ -1,15 +1,19 @@
 Template.rooms.events({
   "submit form#new-message": function(e) {
     e.preventDefault();
-
     var username = Meteor.user().username;
     var currentRoom = Session.get("currentRoom").toString();
     var inputMessage = $("input#message-to-add").val();
-
     Messages.insert({ username: username, message: inputMessage, room: currentRoom });
-
     Meteor.call("ScrollTop");
     $("input#message-to-add").val("");
+  },
+
+  "submit form#room-create-form": function(e) {
+    e.preventDefault();
+    var roomCreate = { name: $("#room-name").val(), description: $("#room-description").val() };
+    Rooms.insert({name: roomCreate.name, description: roomCreate.description});
+    $("#room-create").modal("hide");
   }
 });
 
